55f42dc11661fa4c8bcfbe862515ff0b4843d4e5
Abhishek Arya <arya2001abhishek@gmail.com>
2021-08-16 18:32:29 +0530

    add yargs for input parsing

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./utils.js')
+const getNotes = require('./notes.js')
 
-console.log(yargs.argv)
+// Customize yargs version
+yargs.version('1.1.0')
 
-// create add command
 yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
+    command: 'add', 
+    describe: 'Add a new note', 
     handler: function() {
-        console.log('Adding a new Note!!')
+        console.log('Adding notes');
     }
 })
+
+
+yargs.command({
+    command: 'remove', 
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function() {
+        console.log('Displaying all the notes!')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'reading the notes',
+    handler: function() {
+        console.log('Reading a note')
+    }
+})
+
+yargs.argv
+// imp to run
